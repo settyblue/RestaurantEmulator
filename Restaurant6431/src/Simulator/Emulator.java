cee6387cbd628221eb1cddf3ac2c8476c4e5d2bf
Rakshith Kunchum <settyblue@gmail.com>
2015-12-09 10:34:24 -0800

    corrected package headers.

--- Restaurant6431/src/Simulator/Emulator.java
+++ Restaurant6431/src/Simulator/Emulator.java
@@ -6,11 +6,7 @@ import Timer.*;
 import Output.*;
 
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.FileNotFoundException;
 import java.io.FileReader;
-import java.io.FileWriter;
-import java.io.IOException;
 
 public class Emulator {
 	
